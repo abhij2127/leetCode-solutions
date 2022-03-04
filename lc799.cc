39ec9d7bf979725ceb637264c42d087ddd777387
Abhishek Joshi <abbu.joshi.satya@gmail.com>
2022-03-04 11:27:42 +0530

    Maybe Better

---
+++ lc799.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+       //I seriously don't know the logic\
+       //Pleae forgive. 
+    }
+};
+
+int main(){
+	while(1){
+		int poured, q_row, q_column;
+		cin>>poured>>q_row>>q_column;
+		Solution *S = new Solution();
+		double ans = S->champagneTower(poured, q_row, q_glass);
+		cout<<ans<<endl;
+	}
+	return 0;
+}
+	
